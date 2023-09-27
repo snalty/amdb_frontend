3dc2a6372c822e01c75496fa63ee03d447b3479e
Sam Nalty <nalty.blade@gmail.com>
2023-09-27 22:21:22 +0100

    - Move API endpoint to environment - Support new api endpoint

--- src/views/ResultsView.vue
+++ src/views/ResultsView.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-const BASE_API_URL = "/api/variant"
+const BASE_API_URL = import.meta.env.VITE_API_URL
 import { useRoute } from 'vue-router'
 import { watchEffect, ref } from 'vue';
 import { LinkIcon } from '@heroicons/vue/24/solid'
 
 let route = useRoute()
 
-let results = ref<any>()
+let result = ref<any>()
 let loading = ref(true);
 
 let search = route.params.location.toString();
 let [chrom, pos, ref_allele, alt] = search.split('-');
 let genome = route.params.genome.toString();
+
+console.log(BASE_API_URL)
 
 watchEffect(async () => {
   // this effect will run immediately and then
@@ -21,11 +23,11 @@ watchEffect(async () => {
   [chrom, pos, ref_allele, alt] = search.split('-');
   genome = route.params.genome.toString();
 
-  results.value = null;
+  result.value = null;
 
-  const url = `${BASE_API_URL}?chrom=chr${chrom}&pos=${pos}&ref_allele=${ref_allele}&alt_allele=${alt}&genome=${route.params.genome}`
+  const url = `${BASE_API_URL}/v1/variant?chrom=chr${chrom}&pos=${pos}&ref_allele=${ref_allele}&alt_allele=${alt}&genome=${route.params.genome}`
 
-  results.value = await (await fetch(url)).json()
+  result.value = await (await fetch(url)).json()
 
   loading.value = false;
 })
@@ -40,9 +42,8 @@ watchEffect(async () => {
             <svg class="animate-spin" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/><path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" class="spinner_ajPY"/></svg>
         </div>
         <!-- @vue-ignore -->
-        <div v-else-if="//@ts-ignore 
-        !results.length == 0">
-            <div v-for="result in results" class="bg-white shadow-sm w-full flex justify-evenly md:divide-x-2 divide-y-2 md:divide-y-0 flex-col md:flex-row">
+        <div v-else-if="result">
+            <div class="bg-white shadow-sm w-full flex justify-evenly md:divide-x-2 divide-y-2 md:divide-y-0 flex-col md:flex-row">
                 <div class="py-3 pl-3">
                     <div class="text-sm font-bold text-gray-500">UNIPROT ID:</div>
                     <div class="text-3xl py-3 flex flex-row">
