a2127f5cbfd6cd6504fa6572b82cf7f50ccfdde3
Sam Nalty <nalty.blade@gmail.com>
2025-11-12 12:00:46 +0000

    move api spec to frontend

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ResultsView from '../views/ResultsView.vue'
+const SwaggerView = () => import('@/views/SwaggerView.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/results/:genome/:location/',
       name: 'results',
       component: ResultsView
+    },
+    {
+      path: '/swagger',
+      name: 'swagger',
+      component: SwaggerView
     }
   ]
 })
