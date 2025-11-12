4591e9b61fb756f6c939549be5d1b784b2270588
Sam Nalty <nalty.blade@gmail.com>
2025-11-12 12:13:11 +0000

    update api spec link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,7 +9,7 @@ import { Cog6ToothIcon, CodeBracketSquareIcon } from '@heroicons/vue/24/solid'
     <span class="text-7xl">AMDB</span>
     <span class="text-xl">A tool for querying AlphaMissense results</span>
     <div class="flex md:flex-row flex-col mt-3">
-      <a class="bg-teal-500 p-2 m-2 text-white flex items-center rounded-lg shadow-sm hover:shadow-lg" :href="`${BASE_API_URL}/swagger-ui`"><Cog6ToothIcon class="h-5 w-5 mr-1" /><span>API Docs</span></a>
+      <a class="bg-teal-500 p-2 m-2 text-white flex items-center rounded-lg shadow-sm hover:shadow-lg" href="/swagger"><Cog6ToothIcon class="h-5 w-5 mr-1" /><span>API Docs</span></a>
       <a class="bg-teal-500 p-2 m-2 text-white flex items-center rounded-lg shadow-sm hover:shadow-lg" href="https://github.com/snalty/amdb_frontend"><CodeBracketSquareIcon class="h-5 w-5 mr-1" /><span>Frontend source</span></a>
       <a class="bg-teal-500 p-2 m-2 text-white flex items-center rounded-lg shadow-sm hover:shadow-lg" href="https://github.com/snalty/amdb_backend"><CodeBracketSquareIcon class="h-5 w-5 mr-1" /><span>Backend source</span></a>
     </div>
